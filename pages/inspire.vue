e1a2b9f83cf22fa6503c30a1e67700f25d72212c
sanjarbarakayev <genius.s.1799@gmail.com>
2021-11-01 22:01:55 +0500

    init: create app

---
+++ pages/inspire.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+    <v-col class="text-center">{{ user }} </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const data = await $axios.$get('/api/users')
+    return {
+      user: data,
+    }
+  },
+  head() {
+    return {
+      title: 'Users',
+    }
+  },
+}
+</script>
